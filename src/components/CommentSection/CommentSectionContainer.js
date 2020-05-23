7e9eed64983e8cbb98de2dce537900ddfde4ce75
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-22 18:45:18 -0700

    searchbar added, comment section mvp complete

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,12 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments, useComment] = useState(props.comments)
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(comment => <Comment comment={comment}/>)}
       <CommentInput />
     </div>
   );
