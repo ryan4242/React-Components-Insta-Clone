69568d903787c00b078dc1ea5408dfef0356c0ec
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-24 21:43:33 -0700

    deleted unused useState hooks

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,10 +1,8 @@
 //Complete the necessary code in this file
 // import useState
-import React, {useState} from "react";
+import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
-import dummyData from '../../dummy-data';
 
 const PostsPage = props => {
   return (
--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,5 +1,5 @@
 // You do not need to change any code in this file
-import React, {useState} from "react";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
